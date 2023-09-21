fb8940922c94b3f5cef8eb84063218aa6c5917b4
BigDada1 <48822301+BigDada1@users.noreply.github.com>
2023-09-21 15:40:36 +0200

    Event mailer implemented

---
+++ cms/site/templates/event-mailer.php
@@ -0,0 +1,7 @@
+<?php
+include("./includes/head.inc");
+include("./includes/header.inc");
+include("./includes/basic-content.inc");
+include("./includes/events-func.inc");
+include("./includes/event-mailer.inc");
+include("./includes/foot.inc");
