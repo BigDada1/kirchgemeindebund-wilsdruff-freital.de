18a49a8730799208f656ce1c22f8b210cb01df73
Clemens Henker <c.henker@me.com>
2025-05-08 18:08:39 +0200

    Event list template added to support separate site

---
+++ cms/site/templates/event-list.php
@@ -0,0 +1,7 @@
+<?php
+include("./includes/head.inc");
+include("./includes/header.inc");
+include("./includes/basic-content.inc");
+include("./includes/events-func.inc");
+include("./includes/events.inc");
+include("./includes/foot.inc");
